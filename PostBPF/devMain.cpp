7f077bef93677d4d4be4aa4b54dd3250a8ffd8d0
quanched <quanched@node0.quanched-202920.datacenter-soc-pg0.wisc.cloudlab.us>
2024-04-17 22:50:37 -0500

    add devMain,cpp

--- PostBPF/devMain.cpp
+++ PostBPF/devMain.cpp
@@ -1,6 +1,13 @@
 #include "BPFGrep.h"
+#include <map>
+void add(){
+
+}
 
 int main() {
+    // buffMap[buffer content] -> ptr addr
+    std::map<std::string, int> buffMap;
+
     BPFGrep BPFGrep("test.txt");
     BPFGrep.readArgLabel();
     BPFGrep.readArgPtr();
