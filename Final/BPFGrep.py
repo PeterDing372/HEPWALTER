926b173a540f50657b297c67a27a59c0d865ac87
quanched <quanched@node0.quanched-202920.datacenter-soc-pg0.wisc.cloudlab.us>
2024-04-18 23:47:17 -0500

    divergence, redudancy check, need better data struct for summary and plotting

--- Final/BPFGrep.py
+++ Final/BPFGrep.py
@@ -1,15 +1,26 @@
+import sys
 class BPFGrep:
-    def __init__(self, filename):
+    def __init__(self, filename, verbose=False):
         self.filename = filename
+        self.verbose = verbose  # Class attribute to control printing
         try:
             self.file = open(self.filename, 'r')
         except Exception as e:
             print(f"Unable to open file: {filename}")
             print(e)
+            sys.exit(1)
 
     def __del__(self):
-        if not self.file.closed:
+        if self.file and not self.file.closed:
             self.file.close()
+
+    def read_one_arg(self):
+        label = self.read_arg_label()
+        ptr_addr = self.read_arg_ptr()
+        buffer_content = self.read_buffer_content()
+        return label, buffer_content, ptr_addr
+
+    # ---------- Helper Functions Below ----------
 
     def read_arg_label(self):
         arg_type = ""
@@ -18,8 +29,8 @@ class BPFGrep:
             if ch == ':' or not ch:
                 break
             arg_type += ch
-        print(arg_type)
-        return arg_type[:-1]
+        self._print(arg_type)
+        return arg_type
 
     def read_arg_ptr(self):
         arg_ptr = ""
@@ -28,14 +39,14 @@ class BPFGrep:
             if ch == '\n' or not ch:
                 break
             arg_ptr += ch
-        print(arg_ptr)
+        self._print(arg_ptr)
         return self.to_int(arg_ptr)
 
     def to_int(self, input_str):
         try:
             return int(input_str)
         except ValueError as e:
-            print(f"Invalid argument: {input_str}")
+            self._print(f"Invalid argument: {input_str}")
             return 0
 
     def read_buffer_content(self):
@@ -55,15 +66,9 @@ class BPFGrep:
 
             if self.is_buffer_end(last13_chars):
                 break
-        self.clean_tail(buffer_content, max_chars)
-        print(buffer_content)
+        buffer_content = self.clean_tail(buffer_content, max_chars)
+        self._print(buffer_content)
         return buffer_content
-
-    def read_one_arg(self):
-        label = self.read_arg_label()
-        ptr_addr = self.read_arg_ptr()
-        buffer_content = self.read_buffer_content()
-        return label, buffer_content, ptr_addr
 
     def is_buffer_end(self, input_str):
         return input_str == "**HELALTER***"
@@ -72,5 +77,10 @@ class BPFGrep:
         if len(input_str) >= num_chars_to_remove:
             return input_str[:-num_chars_to_remove]
         else:
-            print("Error: The string is shorter than 13 characters.")
+            self._print("Error: The string is shorter than 13 characters.")
             return input_str
+
+    # Private helper method to control printing
+    def _print(self, message):
+        if self.verbose:
+            print(message)
--- Final/dev.py
+++ Final/dev.py
@@ -1,114 +1,53 @@
-class BPFGrep:
-    def __init__(self, filename, verbose=False):
-        self.filename = filename
-        self.verbose = verbose  # Class attribute to control printing
-        try:
-            self.file = open(self.filename, 'r')
-        except Exception as e:
-            self._print(f"Unable to open file: {filename}")
-            self._print(e)
-
-    def __del__(self):
-        if self.file and not self.file.closed:
-            self.file.close()
-
-    def read_one_arg(self):
-        label = self.read_arg_label()
-        ptr_addr = self.read_arg_ptr()
-        buffer_content = self.read_buffer_content()
-        return label, buffer_content, ptr_addr
-
-    # ---------- Helper Functions Below ----------
-
-    def read_arg_label(self):
-        arg_type = ""
-        while True:
-            ch = self.file.read(1)
-            if ch == ':' or not ch:
-                break
-            arg_type += ch
-        self._print(arg_type)
-        return arg_type[:-1]
-
-    def read_arg_ptr(self):
-        arg_ptr = ""
-        while True:
-            ch = self.file.read(1)
-            if ch == '\n' or not ch:
-                break
-            arg_ptr += ch
-        self._print(arg_ptr)
-        return self.to_int(arg_ptr)
-
-    def to_int(self, input_str):
-        try:
-            return int(input_str)
-        except ValueError as e:
-            self._print(f"Invalid argument: {input_str}")
-            return 0
-
-    def read_buffer_content(self):
-        buffer_content = ""
-        last13_chars = ""
-        max_chars = 13
-
-        while True:
-            ch = self.file.read(1)
-            if not ch:
-                break
-            buffer_content += ch
-            last13_chars += ch
-
-            if len(last13_chars) > max_chars:
-                last13_chars = last13_chars[1:]
-
-            if self.is_buffer_end(last13_chars):
-                break
-        buffer_content = self.clean_tail(buffer_content, max_chars)
-        self._print(buffer_content)
-        return buffer_content
-
-    def is_buffer_end(self, input_str):
-        return input_str == "**HELALTER***"
-
-    def clean_tail(self, input_str, num_chars_to_remove):
-        if len(input_str) >= num_chars_to_remove:
-            return input_str[:-num_chars_to_remove]
-        else:
-            self._print("Error: The string is shorter than 13 characters.")
-            return input_str
-
-    # Private helper method to control printing
-    def _print(self, message):
-        if self.verbose:
-            print(message)
-
+from BPFGrep import BPFGrep
 
 
 
 def main():
     # Example usage:
-    bpfgrep_obj = BPFGrep("test.txt", verbose=False)
-    buff_map = {}
-    num_elements = 3
+    bpfgrep_obj = BPFGrep("test_redundant.txt", verbose=False)
+    buff_tuples = [] # (ptr_addr, buffer_content, label)
+    num_elements = 6
     # Read through complete file
     for _ in range(num_elements):
         label, buffer_content, ptr_addr = bpfgrep_obj.read_one_arg()
+        buff_tuples.append((ptr_addr, buffer_content, label))
 
-        if buffer_content in buff_map:
-            buff_map[buffer_content].append(ptr_addr)
-        else:
-            buff_map[buffer_content] = [ptr_addr]
-    printBuffMap(buff_map)
+    printTupleList(buff_tuples)
+    compare_all_buff(buff_tuples)
     
 
-def printBuffMap(buff_map):
+def printTupleList(tuples_list):
+    attributes = ["ptr addr", "buffer content", "label"]
     # Print the contents of the map
-    for buffer_content, ptr_addrs in buff_map.items():
-        print(f"Buffer Content: {buffer_content} -> Pointer Addresses: ", end='')
-        print(" ".join(map(str, ptr_addrs)))
+    for index, tup in enumerate(tuples_list):
+        # Convert each tuple to a string and join with commas for nicer output
+        print(f"Tuple {index + 1}:")
+        for i, item in enumerate(tup):
+            print(f"    {attributes[i]}: {item}")
+        print()  # Add a blank line for better separation between tuples
 
+# Compare all buff content and 
+# returns an index list of tuples containing (i,j) pairs and divergence index
+def compare_all_buff(tupleList, div_threshold=3):
+    size = len(tupleList)
+    for i in range(size):
+        for j in range(i + 1, size):
+            divege_index = find_divergence(tupleList[i][1], tupleList[j][1])
+            if (divege_index >= div_threshold):
+                print(f"same buff arg{i} arg{j} div_index: {divege_index}")
+                if (tupleList[i][0] != tupleList[j][0]):
+                    print("is redundant")
 
+def find_divergence(str1, str2):
+    min_length = min(len(str1), len(str2))
+    for i in range(min_length):
+        if str1[i] != str2[i]:
+            return i  
+    
+    # If no differences were found within the shorter length, 
+    # return the length of the shorter string
+    return min_length
+    
 
 if __name__ == "__main__":
     main()
