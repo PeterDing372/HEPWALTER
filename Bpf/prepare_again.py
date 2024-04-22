db01842242a90c70c07a1f4d54ae079618294a62
GoodData <GoodData@node0.dtrdnt-203508.reducethebuffer-pg0.wisc.cloudlab.us>
2024-04-22 16:14:26 -0500

    Hank added

--- Bpf/prepare_again.py
+++ Bpf/prepare_again.py
@@ -13,9 +13,9 @@ binary = "/usr/bin/mongod"
 Source = "mongo::DecorationRegistry<mongo::OperationContext>::constructAt<boost::optional<mongo::BSONArray"
 # dest unmangled name
 Dest = "mongo::OperationContext::OperationContext(mongo::Client"
-bufflength = 20
+bufflength = 100
 #ptr_incrementlength = 1
-ptr_incrementAmount = 20
+ptr_incrementAmount = 100
 
 #Don't modify these names
 mangledfilename = "manglednames.txt"
@@ -41,10 +41,10 @@ print(f"container name is {container_name} and PID is {pid}")
 
 def create_objdumps(path_to_binary):
 
-    subprocess.check_output(['sudo', 'objdump', '-CT', path_to_binary, '>', 'unmangledmanglednames.txt'])
-    subprocess.check_output(['sudo', 'objdump', '-T', path_to_binary, '>', 'manglednames.txt'])
+    unmangled = subprocess.check_output(['sudo', 'objdump', '-CT', path_to_binary])
+    mangled = subprocess.check_output(['sudo', 'objdump', '-T', path_to_binary])
 
-    return
+    return unmangled.decode('utf-8'), mangled.decode('utf-8')
 
 def find_mangled_name(objdump_T_output, objdump_CT_output, unmangled_name):
     mangled_name = None
@@ -110,29 +110,30 @@ def args_string(type_args ,ptrIncrementLength, incrementAmount):
         increment = 0
         if type_args.lower() == "source":
             if currPtrIncrementLength <= 0:
-                curr_string += "\tprintf(\"sArgJESSE:%zu\\n%r**HEPWALTER***\\n\", argJESSE, buf(argJESSE, 20));\n\n"
+                curr_string += "\tprintf(\"sArgJESSE:%zu\\n%r**HEPWALTER***\\n\", argJESSE, buf(argJESSE, HANK));\n\n"
             else:
-                curr_string += "\tprintf(\"sArgJESSE:%zu\\n%r\", argJESSE, buf(argJESSE, 20));\n"
+                curr_string += "\tprintf(\"sArgJESSE:%zu\\n%r\", argJESSE, buf(argJESSE, HANK));\n"
         elif type_args.lower() == "destination":
             if currPtrIncrementLength <= 0:
-                curr_string += "\tprintf(\"dArgJESSE:%zu\\n%r**HEPWALTER***\\n\", argJESSE, buf(argJESSE, 20));\n\n"
+                curr_string += "\tprintf(\"dArgJESSE:%zu\\n%r**HEPWALTER***\\n\", argJESSE, buf(argJESSE, HANK));\n\n"
             else:
-                curr_string += "\tprintf(\"dArgJESSE:%zu\\n%r\", argJESSE, buf(argJESSE, 20));\n"
+                curr_string += "\tprintf(\"dArgJESSE:%zu\\n%r\", argJESSE, buf(argJESSE, HANK));\n"
 
 
         while(currPtrIncrementLength):
             increment += incrementAmount
             if currPtrIncrementLength == 1:
                 #if type_args.lower() == "source":
-                curr_string += "\tprintf(\"%r**HEPWALTER***\\n\", buf(argJESSE+" + str(increment) + ", 20));\n\n"
+                curr_string += "\tprintf(\"%r**HEPWALTER***\\n\", buf(argJESSE+" + str(increment) + ", HANK));\n\n"
             else:
-                curr_string += "\tprintf(\"%r\", buf(argJESSE+" + str(increment) + ", 20));\n"
+                curr_string += "\tprintf(\"%r\", buf(argJESSE+" + str(increment) + ", HANK));\n"
             
 
             currPtrIncrementLength -= 1
 
         final_string += curr_string
         final_string = final_string.replace("JESSE", str(arg_num))
+        final_string = final_string.replace("HANK", str(bufflength))
 
 
     return final_string
@@ -147,14 +148,14 @@ def retval_string(type_args ,ptrIncrementLength, incrementAmount):
     increment = 0
     if type_args.lower() == "source":
         if currPtrIncrementLength <= 0:
-            curr_string += "\tprintf(\"sRetVal:%zu\\n%r**HEPWALTER***\\n\", retval, buf(retval, 20));\n\n"
+            curr_string += "\tprintf(\"sRetVal:%zu\\n%r**HEPWALTER***\\n\", retval, buf(retval, HANK));\n\n"
         else:
-            curr_string += "\tprintf(\"sRetVal:%zu\\n%r\", retval, buf(retval, 20));\n"
+            curr_string += "\tprintf(\"sRetVal:%zu\\n%r\", retval, buf(retval, HANK));\n"
     elif type_args.lower() == "destination":
         if currPtrIncrementLength <= 0:
-            curr_string += "\tprintf(\"dRetval:%zu\\n%r**HEPWALTER***\\n\", retval, buf(retval, 20));\n\n"
+            curr_string += "\tprintf(\"dRetval:%zu\\n%r**HEPWALTER***\\n\", retval, buf(retval, HANK));\n\n"
         else:
-            curr_string += "\tprintf(\"dRetval:%zu\\n%r\", retval, buf(retval, 20));\n"
+            curr_string += "\tprintf(\"dRetval:%zu\\n%r\", retval, buf(retval, HANK));\n"
 
 
     while(currPtrIncrementLength):
@@ -162,9 +163,9 @@ def retval_string(type_args ,ptrIncrementLength, incrementAmount):
         
         if currPtrIncrementLength == 1:
             #if type_args.lower() == "source":
-            curr_string += "\tprintf(\"%r**HEPWALTER***\\n\", buf(retval+" + str(increment) + ", 20));\n\n"
+            curr_string += "\tprintf(\"%r**HEPWALTER***\\n\", buf(retval+" + str(increment) + ", HANK));\n\n"
         else:
-            curr_string += "\tprintf(\"%r\", buf(retval+" + str(increment) + ", 20));\n"
+            curr_string += "\tprintf(\"%r\", buf(retval+" + str(increment) + ", HANK));\n"
         
         
         currPtrIncrementLength -= 1
@@ -173,6 +174,7 @@ def retval_string(type_args ,ptrIncrementLength, incrementAmount):
         curr_string = ""
         
 
+    final_string = final_string.replace("HANK", str(bufflength))
 
     return final_string
             
@@ -187,13 +189,13 @@ if __name__ == "__main__":
 
     path_to_binary = f"/proc/{pid}/root/{binary}"
 
-    create_objdumps(path_to_binary)
+    objdump_CT_output, objdump_T_output = create_objdumps(path_to_binary)
 
-    with open(mangledfilename, "r") as f:
-        objdump_T_output = f.read()
+    #with open(mangledfilename, "r") as f:
+    #    objdump_T_output = f.read()
 
-    with open(unmangledfilename, "r") as f:
-        objdump_CT_output = f.read()
+    #with open(unmangledfilename, "r") as f:
+    #    objdump_CT_output = f.read()
 
     mangled_source_name = find_mangled_name(objdump_T_output, objdump_CT_output, Source)
     mangled_dest_name = find_mangled_name(objdump_T_output, objdump_CT_output, Dest)
@@ -212,3 +214,4 @@ if __name__ == "__main__":
     replace_in_file("final_run_again.bt", "final_run_again.bt", "DARGS", destination_args_string)
     replace_in_file("final_run_again.bt", "final_run_again.bt", "SRETVAL", source_retval_string)
     replace_in_file("final_run_again.bt", "final_run_again.bt", "DRETVAL", destination_retval_string)
+
